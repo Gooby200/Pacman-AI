e80cc28eabc3c0562f76825c983ff9d29742ba61
Gooby200 <rgpesa@gmail.com>
2017-09-18 16:58:10 -0500

    added intersections and their properties

---
+++ Assets/Scripts/Intersection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intersection : MonoBehaviour {
+
+    public bool Left = false;
+    public bool Right = false;
+    public bool Up = false;
+    public bool Down = false;
+
+}
