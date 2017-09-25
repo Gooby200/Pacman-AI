8a0e1f6ecb1cbf4dcbb5ab6082da57056dfd4b97
Gooby200 <rgpesa@gmail.com>
2017-09-24 22:13:54 -0500

    began work on MDP algorithm

---
+++ Assets/Scripts/MDP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MDP : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+
+		int rows = 20;
+		int columns = 20;
+	
+		float tileWidth = (this.transform.localScale.x * 10) / columns;
+		float tileHeight = (this.transform.localScale.z * 10) / rows;
+
+		Gizmos.DrawWireCube (new Vector3 (0, 0, 0), new Vector3 (tileWidth, 2f, tileHeight));
+
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < columns; c++) {
+				//Gizmos.DrawCube (new Vector3 (c, 2f, r), new Vector3 (rows, 2f, columns));
+			}
+		}
+
+
+	}
+}
--- Assets/Scripts/PacmanAI.cs
+++ Assets/Scripts/PacmanAI.cs
@@ -3,27 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PacmanAI : MonoBehaviour {
-	public Vector3 FacingPosition;
 	public int FacingDirection = 2;
-
-	public int MovingDirection = 2;
-
-	// Update is called once per frame
-	void Update () {
-		//do AI stuff here (markov decision process?)
-
-		//update the position of the location that pacman is facing
-		if (MovingDirection == 1) {
-			FacingPosition = new Vector3 (this.transform.position.x - 0.3757539f, 0, this.transform.position.z);
-		} else if (MovingDirection == 2) {
-			FacingPosition = new Vector3 (this.transform.position.x + 0.3757539f, 0, this.transform.position.z);
-		} else if (MovingDirection == 3) {
-			FacingPosition = new Vector3 (this.transform.position.x, 0, this.transform.position.z + 0.3757539f);
-		} else if (MovingDirection == 4) {
-			FacingPosition = new Vector3 (this.transform.position.x, 0, this.transform.position.z - 0.3757539f);
-		}
-
-		//update the facing direction (left, right, up or down)
-		FacingDirection = MovingDirection;
-	}
 }
